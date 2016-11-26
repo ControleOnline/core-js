17249159a746ec8191d44192908a4d7b4ee080c6
Luiz Kim <luizkim@gmail.com>
2016-11-26 21:48:06 -0200

    Add fontawesome on dependencies

--- controleonline-core.js
+++ controleonline-core.js
@@ -1,1 +1,19 @@
-alert('test');
+requirejs.config({
+    baseUrl: '/vendor',
+    paths: {
+        jquery: 'jquery/dist/jquery.min',
+        bootstrap: 'bootstrap/dist/js/bootstrap.min',
+        datatables: 'datatables/media/jquery.dataTables.min',
+        highcharts: 'highcharts/highcharts'        
+    },
+    map: {
+        '*': {'jquery': 'jquery-private'},
+        'jquery-private': {'jquery': 'jquery'}
+    }
+});
+define(['jquery'], function (jq) {
+    return jq.noConflict(true);
+});
+requirejs(['jquery'], function ($) {
+    console.log($);
+});
